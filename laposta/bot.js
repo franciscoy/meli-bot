52bcf55804fabeeda51ec447fcafa6f75b40d793
nicofaure <nicolas.faure@mercadolibre.com>
2016-11-02 18:27:09 -0300

    Add different messages format

--- laposta/bot.js
+++ laposta/bot.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var server = express();
-var page_token = "EAAEbEbJAC78BAMznWNyMVS0GD7TKLYBhuCSK74QFphfnkZBoILKzZBKLh31wFM7W6lK7jdQbYmkIocbV2xYZCHMem6CvHWJRCmMVJDZAZBNbEm8PvZAsqDHamVlsVhaRaAOl3id6abpRh1dxStIFaMAXdhQXktPuV34jgESh5CJwZDZD";
+var page_token = "";
 var SERVER_PORT = 8080;
 var bodyParser = require('body-parser');
 var request = require('request');
@@ -14,9 +14,53 @@ server.get('/hello',function(req,res){
 
 server.post('/hello',function(req,res){
 	console.log(req.body.entry[0].messaging[0]);
-  sendTextMessage(req.body.entry[0].messaging[0].sender.id, 'hola mundo');
+  var entry = req.body.entry[0].messaging[0];
+  var textMessage = entry.message.text;
+  if(textMessage=="imagen"){
+    sendImage(entry.sender.id);
+  }else if(textMessage=="video"){
+    sendVideo(entry.sender.id);
+  }else{
+    sendGenericMessage(req.body.entry[0].messaging[0].sender.id);  
+  }
+  
   res.sendStatus(200);
 });
+
+
+function sendVideo(recipientId){
+  var imageData = {
+    recipient:{
+      id: recipientId
+    },
+    message:{
+      attachment:{
+        type:"video",
+        payload:{
+          url:"http://techslides.com/demos/sample-videos/small.mp4"
+        }
+      }
+    }
+  }
+  callSendAPI(imageData);
+}
+
+function sendImage(recipientId){
+  var imageData = {
+    recipient:{
+      id: recipientId
+    },
+    message:{
+      attachment:{
+        type:"image",
+        payload:{
+          url:"http://www.pngmart.com/files/2/Drone-PNG-Pic.png"
+        }
+      }
+    }
+  }
+  callSendAPI(imageData);
+}
 
 function sendTextMessage(senderId, messageText) {
   var messageData = {
@@ -51,6 +95,53 @@ function callSendAPI(messageData) {
       console.error(error);
     }
   });  
+}
+
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
 }
 
 server.listen(SERVER_PORT);
